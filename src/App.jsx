6db3e0e3ddf4a9182983582e4fd39f23346580b6
Bulat Murtazin <bulatf.murtazin@gmail.com>
2021-08-05 14:27:09 +0300

    = fix import order in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 
 import { AUTH_ROUTES_BASE } from './routes/auth';
 import { STREAMS_ROUTES_BASE } from './routes/streams';
+import { INTERNS_ROUTES_BASE } from './routes/interns';
 import Auth from './views/Auth';
 import Streams from './views/Streams';
-
-import './assets/styles/index.css';
-import { INTERNS_ROUTES_BASE } from './routes/interns';
 import Interns from './views/Interns';
 import MainLayout from './components/MainLayout';
+
+import './assets/styles/index.css';
 
 const App = () => (
   <BrowserRouter>
