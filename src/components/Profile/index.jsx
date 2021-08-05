512230f6e2b52d84b07b0652725a47aa6502acb6
Bulat Murtazin <bulatf.murtazin@gmail.com>
2021-08-05 12:26:52 +0300

    = finish Profile popup markup

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import classNames from 'classnames';
 import avatar from '../../assets/images/avatar.png';
 import styles from './Profile.module.css';
@@ -8,13 +8,29 @@ const userName = 'Jon Snow';
 
 const Profile = () => {
   const [showPopup, setShowPopup] = useState(false);
-  const handleClick = () => {
+
+  const onTriggerClick = useCallback((e) => {
+    e.stopPropagation();
     setShowPopup((shown) => !shown);
-  };
+  }, []);
+
+  useEffect(() => {
+    if (showPopup) {
+      document.addEventListener('click', onTriggerClick);
+    }
+
+    return () => {
+      document.removeEventListener('click', onTriggerClick);
+    };
+  }, [onTriggerClick, showPopup]);
 
   return (
     <div className={styles.container}>
-      <Button className={styles.profile} variant="link" onClick={handleClick}>
+      <Button
+        className={styles.profile}
+        variant="link"
+        onClick={onTriggerClick}
+      >
         <>
           <img className={styles.avatar} src={avatar} alt="Avatar" />
           <span className={styles.userName}>{userName}</span>
