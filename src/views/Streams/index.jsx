edabc9fe7f5b05f2f3aaa33484d21aeabd00a1ed
Bulat Murtazin <bulatf.murtazin@gmail.com>
2021-08-08 14:12:41 +0300

    = use AddStream modal on Streams page

--- src/views/Streams/index.jsx
+++ src/views/Streams/index.jsx
@@ -1,32 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import mockStreams from '../../mock/streams';
 import Button from '../../components/Button';
 import StreamsTabs from '../../components/StreamsTabs';
 import Table from '../../components/Table/Table';
+import AddStream from '../../modal/AddStream';
 import { streamsTableCols } from './constants';
 import styles from './Streams.module.css';
-import Modal from '../../components/Modal';
 
-const Streams = () => (
-  <>
-    <header className={styles.header}>
-      <h1 className={styles.title}>Internship Streams</h1>
-      <Button className={styles.addStreamButton}>+ Add Stream</Button>
-    </header>
-    <nav className={styles.nav}>
-      <StreamsTabs />
-    </nav>
-    <div className={styles.content}>
-      <Table
-        columns={streamsTableCols}
-        dataSource={mockStreams}
-        hasPagination
+const Streams = () => {
+  const [showAddStream, setShowAddStream] = useState(false);
+  return (
+    <>
+      <header className={styles.header}>
+        <h1 className={styles.title}>Internship Streams</h1>
+        <Button
+          className={styles.addStreamButton}
+          onClick={() => {
+            setShowAddStream(true);
+          }}
+        >
+          + Add Stream
+        </Button>
+      </header>
+      <nav className={styles.nav}>
+        <StreamsTabs />
+      </nav>
+      <div className={styles.content}>
+        <Table
+          columns={streamsTableCols}
+          dataSource={mockStreams}
+          hasPagination
+        />
+      </div>
+      <AddStream
+        visible={showAddStream}
+        onClose={() => {
+          setShowAddStream(false);
+        }}
       />
-    </div>
-    <Modal title="Add Stream" visible>
-      Hello
-    </Modal>
-  </>
-);
+    </>
+  );
+};
 
 export default Streams;
