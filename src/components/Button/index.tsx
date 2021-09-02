d6201f9f7bf3e5184c1db09bef5b06bf3222d17c
Bulat Murtazin <bulatf.murtazin@gmail.com>
2021-09-02 16:51:22 +0300

    = rewrite Button component using typescript

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,43 @@
+/* eslint-disable react/button-has-type */
+import React, { FC, SyntheticEvent } from 'react';
+import classNames from 'classnames';
+import styles from './Button.module.css';
+
+type ButtonProps = {
+  children: React.ReactNode;
+  className?: string;
+  variant?: 'primary' | 'outline' | 'link';
+  type?: 'button' | 'submit';
+  onClick?: (event: SyntheticEvent<HTMLButtonElement>) => void;
+  'data-testid'?: string;
+};
+
+const Button: FC<ButtonProps> = ({
+  children,
+  className = '',
+  variant = 'primary',
+  type = 'button',
+  onClick = undefined,
+  'data-testid': dataTestId = 'button',
+}) => {
+  const resultClassName = classNames({
+    [styles.button]: true,
+    [styles.primary]: variant === 'primary',
+    [styles.outline]: variant === 'outline',
+    [styles.link]: variant === 'link',
+    [className]: true,
+  });
+
+  return (
+    <button
+      data-testid={dataTestId}
+      className={resultClassName}
+      type={type}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
